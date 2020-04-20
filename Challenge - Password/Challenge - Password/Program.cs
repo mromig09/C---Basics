cbce277fe99fe5954601a8c4f1419216bb05fb57
mromig09 <mromig09@users.noreply.github.com>
2020-04-21 02:00:15 +1000

    challenge 2 done

---
+++ Challenge - Password/Challenge - Password/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge___Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Username:");
+            var username = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            var password = Console.ReadLine();
+
+            if (password == "secret")
+            {
+                Console.WriteLine("That's Correct!!");
+            }
+
+            else if (password != "secret")
+            {
+                Console.WriteLine("That is incorrect!!");
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output variable 
+            //var a = "Hello World!";
+
+            //input variable
+            var a = Console.ReadLine();
+            Console.WriteLine(a);
+            
+            Console.ReadKey();
+        }
+    }
+}
